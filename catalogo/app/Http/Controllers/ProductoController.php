c6c2df689b1aa82b574451552641aa6e78685451
Marcos Pinardi <exegeses@gmail.com>
2021-03-30 11:38:52 -0300

    formulario para agregar un producto

--- catalogo/app/Http/Controllers/ProductoController.php
+++ catalogo/app/Http/Controllers/ProductoController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Categoria;
+use App\Models\Marca;
 use App\Models\Producto;
 use Illuminate\Http\Request;
 
@@ -28,7 +30,15 @@ class ProductoController extends Controller
      */
     public function create()
     {
-        //
+        //obtenemos listados de marcas y categorías
+        $marcas = Marca::all();
+        $categorias = Categoria::all();
+        return view('agregarProducto',
+                    [
+                        'marcas'=>$marcas,
+                        'categorias'=>$categorias
+                    ]
+                );
     }
 
     /**
